00b7f3b18508ff655111991a5b0b59ef58c6d138
yuxi-fan <yuxifan1@gmail.com>
2022-04-25 15:35:48 -0700

    change graph title

--- apps/visualization.py
+++ apps/visualization.py
@@ -9,17 +9,17 @@ import streamlit.components.v1 as components
 def app():
     option = st.selectbox(
      'Choose the visualization',
-     ('COVID vs Unemployment Claims','Employment level of different size company', 'Employment level of different industry',"Unemployment Claims",'COVID vs Employment Level'))
+     ('COVID vs Unemployment Claims','Employment level change of different size company', 'Employment level change of different industry',"Unemployment Claims",'COVID vs Employment Level'))
 
-    if option == 'Employment level of different size company':
-        st.title("Employment level of different size company")
+    if option == 'Employment level change of different size company':
+        st.title("Employment level change of different size company")
         HtmlFile = open("frontend/employment_company.html", 'r', encoding='utf-8')
         source_code = HtmlFile.read() 
         print(source_code)
         components.html(source_code,width = 1200,height = 900)
 
-    if option == 'Employment level of different industry':
-        st.title("Employment level of different industry")
+    if option == 'Employment level change of different industry':
+        st.title("Employment level change of different industry")
         HtmlFile = open("frontend/employment_industry.html", 'r', encoding='utf-8')
         source_code = HtmlFile.read() 
         print(source_code)
